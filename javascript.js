ac68eaafa28fc654dd5d88562dbf3781456b7921
Elian_Oliveira <elianoliveira234@gmail.com>
2023-02-01 17:55:51 -0300

    mapa interativo funcionando
    agora ao clicar nas regioes, as informacoes correspondentes aparecem na div do lado

--- javascript.js
+++ javascript.js
@@ -1,7 +1,23 @@
 var info = document.querySelector('#info')
 
 function clicouno(regiao){
-    if(regiao == 'sul'){
-        info.innerHTML = '<h2>Região Sul</h2>'
+    if(regiao == 'centro'){
+        info.innerHTML = '<h2>CENTRO</h2>'
+    } else if(regiao == 'docas'){
+        info.innerHTML = '<h2>DOCAS</h2>'
+    } else if(regiao == 'porto'){
+        info.innerHTML = '<h2>PORTO</h2>'
+    } else if(regiao == 'hangar'){
+        info.innerHTML = '<h2>HANGAR</h2>'
+    } else if(regiao == 'sul'){
+        info.innerHTML = '<h2>SUL</h2>'
+    } else if(regiao == 'centro-publico'){
+        info.innerHTML = '<h2>CENTRO PÚBLICO</h2>'
+    } else if(regiao == 'oeste'){
+        info.innerHTML = '<h2>OESTE</h2>'
+    } else if(regiao == 'norte'){
+        info.innerHTML = '<h2>NORTE</h2>'
+    } else {
+        info.innerHTML = '<h2>LESTE</h2>'
     }
 }
